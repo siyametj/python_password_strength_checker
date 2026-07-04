c0a2a45e343c01da1bc21823dc8a93463101636b
usep-arch <s93753564@gmail.com>
2026-07-04 22:59:29 +0600

    feat: password checker of this project

---
+++ src/password_checker.py
@@ -0,0 +1,48 @@
+# src/password_checker.py
+
+import re
+
+class PasswordChecker:
+    def check_strength(self, password: str) -> dict:
+        score = 0
+        feedback = []
+        strength = ""
+
+        if len(password) >= 8:
+            score += 1
+        else:
+            feedback.append("The password must be at least eight characters.")
+
+        if re.search(r'[A-Z]', password):
+            score += 1
+        else:
+            feedback.append("Must have at least one uppercase letter (A-Z)")
+
+        if re.search(r'[a-z]', password):
+            score += 1
+        else:
+            feedback.append("Must have at least one lowercase letter (a-z)")
+
+        if re.search(r'\d', password):
+            score += 1
+        else:
+            feedback.append("Must have at least one number (0-9).")
+
+        if re.search(r'[@$!%*?&#.]', password):
+            score += 1
+        else:
+            feedback.append("Must have at least one special character (e.g.:@, #, $, %)")
+
+        if score <= 2:
+            strength = "Weak"
+        elif score <= 4:
+            strength = "Medium"
+        else:
+            strength = "Strong"
+
+        return {
+            "length": len(password),
+            "score": score,
+            "strength": strength,
+            "feedback": feedback if feedback else ['Your password is perfect']
+        }
